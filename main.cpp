917378fd99f2aec98781f401489364587868e50c
ShenEternity <3258551611@qq.com>
2025-06-16 19:06:13 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "utils\szenext.hpp"
 #include <thread>
 int main(){
@@ -18,18 +17,4 @@ int main(){
     config.ReadConfig();
 
     config.print(); */
-=======
-#include "utils\szenext.hpp"
-#include <thread>
-int main(){
-    Utils utils;
-    SZENEXT sze;
-    utils.clearLog();
-    sze.iniit();
-    std::thread re (&SZENEXT::Read,&sze); 
-    sleep(5);
-    std::thread pd (&SZENEXT::PDMODS,&sze);
-    pd.join();
-    re.join();
->>>>>>> 80eb3b0d70d08b16b33fe6226096c0c8fafeb3e0
 }
